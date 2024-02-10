09d6a45a464843e5f66b3a58580736efa9fa8659
Mostafa_Asaad <ma4190243@gmail.com>
2024-02-10 23:31:12 +0200

    clear_function() is tested

--- test.py
+++ test.py
@@ -9,6 +9,14 @@ with open("Tasks.json", "r") as jsonFile:
     new_tasks:dict = json.load(jsonFile)
 
 # --------------- Printing The content of the json file --------------- #
+print("============= :: ADDING THE TASK :: ============= ")
 print(tabulate(new_tasks))
 # if u don't have tabulate, just use :
 # print(new_tasks)
+
+
+print("============= :: REMOVING THE TASK :: ============= ")
+ret = remove_task(task_temp)
+with open("Tasks.json", "r") as jsonFile:
+    new_tasks:dict = json.load(jsonFile)
+print(tabulate(new_tasks))
